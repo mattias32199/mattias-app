faf96c72b06572e226385e0c40f455f75d9fd30d
MATTIAS HOLLMANN <mattias.hollmann@mercedes-benz.com>
2024-04-19 16:31:21 -0700

    0xA10190

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,145 @@
+<script>
+    import Navbar from '$lib/components/NavBar.svelte';
+</script>
+
+<!-- <svelte:body use:cssVariables={{ background: '#0a0908' }} /> -->
+
+<!-- Navbar component, passing the current page's pathname as a prop. -->
+<Navbar/>
+
+<!-- Where the child components go e.g. +page.svelte -->
+<slot />
+
+<style>
+    * {
+	    box-sizing: border-box;
+    }
+
+    @font-face {
+        font-family: 'Fira Code', monospace;
+        font-display: optional; /* Allows the browser to use a fallback font until 'Fira Code' is downloaded */
+        src: url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap');
+    }
+
+    :global(#svelte) {
+		width: 100vw;
+		height: 100%;
+		max-width: 900px;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+    :global(html),
+
+    /* layout and font styling */
+	:global(body) {
+		transition: background-color 0.2s ease 0s;
+		position: relative;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		font-family: 'Fira Code', monospace;
+		background-color: #0a0908;
+	}
+
+    /* color, margin, additional layout properties */
+    :global(body) {
+		background-color: var(--background);
+		background-size: 200% 200%;
+		color: white;
+		margin: 0;
+		box-sizing: border-box;
+		display: grid;
+		line-height: 1.75;
+		place-items: center;
+		height: 100%;
+		overflow-x: hidden;
+	}
+
+    :global(h1) {
+		border: 0;
+	}
+
+    :global(::selection) {
+		color: white;
+		background: #ca3c25;
+	}
+
+    :global(::-webkit-scrollbar) {
+		width: 8px;
+		height: 8px;
+		border-radius: 1px;
+	}
+
+    :global(::-webkit-scrollbar-thumb) {
+		background-color: #fafffd;
+		border-radius: 3px;
+	}
+
+    :global(::-webkit-scrollbar-track) {
+		background-color: transparent;
+		border-radius: 1px;
+	}
+
+    @media (min-width: 900px) {
+		:global(body) {
+			padding: 0 100px;
+		}
+	}
+
+    :global(a) {
+		text-decoration: none;
+	}
+
+    /* Styles for anchor tags, setting color and removing text-decoration */
+    a {
+        color: rgb(0, 100, 200);
+        text-decoration: none;
+    }
+
+    /* Adds underline to anchor tags on hover */
+    a:hover {
+	    text-decoration: underline;
+    }
+
+    /* Styles for visited anchor tags, changing the color */
+    a:visited {
+	    color: rgb(0, 80, 160);
+    }
+
+    /* Styles for the footer, setting font properties, layout, and alignment */
+    footer {
+        font-size: 16px;
+        font-weight: 400;
+        padding: 30px 0;
+        max-width: 900px;
+        text-align: center;
+        width: 100%;
+    }
+
+    /* Styles for anchor tags inside the footer, setting color and removing text-decoration */
+    footer a {
+	    text-decoration: none;
+	    color: #4158d0;
+    }
+
+    /* Styles for elements with the .svelte class inside the footer, changing the color */
+    footer .svelte {
+	    olor: #ff3e00;
+    }
+
+    /* Styles for elements with the .me class inside the footer, changing the color */
+    footer .me {
+	    color: #ff3e00;
+    }
+
+    /* Media query that makes .tooltip elements visible on wider screens */
+    @media (min-width: 900px) {
+        :global(.tooltip) {
+            visibility: visible;
+        }
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,140 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
+	import FaGithub from 'svelte-icons/fa/FaGithub.svelte';
+	import FaEnvelope from 'svelte-icons/fa/FaEnvelope.svelte';
+	import FaMedium from 'svelte-icons/fa/FaMedium.svelte';
+	import { modalOpened } from '$lib/store';
+</script>
+
+<svelte:head>
+	<title>My Name - Main page</title>
+</svelte:head>
+
+<main>
+	<h1>Mattias<br />Electrical Engineer <br /> Mercedes</h1>
+	<div class="icons">
+		<div
+			role="button"
+			tabindex="0"
+			on:keypress={() => {
+				modalOpened.set(true);
+			}}
+			on:click={() => {
+				modalOpened.set(true);
+			}}
+		>
+			<div class="icon">
+				<FaEnvelope />
+			</div>
+		</div>
+
+		<a
+			href="https://github.com/mattias32199/"
+			aria-label="GitHub"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<div class="icon">
+				<FaGithub />
+			</div>
+		</a>
+		<a
+			href="https://www.linkedin.com/mattiashollmann"
+			aria-label="Linkedin"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<div class="icon">
+				<FaLinkedin />
+			</div>
+		</a>
+		<a
+			href="https://medium.com/"
+			aria-label="Medium"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<div class="icon">
+				<FaMedium />
+			</div>
+		</a>
+	</div>
+</main>
+
+<style>
+    
+    @font-face {
+        font-family: "Bajern";
+        src: url('/fonts/Bajern.otf') format('opentype');
+    }
+
+	a {
+		color: white;
+		text-decoration: none;
+	}
+	main {
+		text-align: center;
+		padding: 0;
+		margin: 0 auto;
+		text-align: center;
+
+		display: flex;
+		flex-direction: column;
+		height: calc(100vh - 80px - 88px);
+		justify-content: center;
+		align-items: center;
+	}
+
+	h1 {
+		font-weight: 700;
+        font-family: 'Bajern';
+	}
+
+	main > h1 {
+		margin: 50px 10px 0;
+		font-size: 36px;
+	}
+
+	.icons {
+		display: flex !important;
+		justify-content: center !important;
+		align-items: center;
+		gap: 20px;
+		cursor: pointer;
+		font-size: 30px;
+		display: flex;
+		justify-content: space-between;
+		max-width: 200px;
+		margin: 50px auto 0;
+	}
+
+	.icon {
+		cursor: pointer;
+		transition: color 0.2s ease-in-out;
+		width: 40px;
+	}
+	.icon:hover {
+		color: #ca3c25;
+	}
+
+	@media (min-width: 900px) {
+		main > h1 {
+			font-size: 48px;
+		}
+	}
+
+	@media (min-width: 600px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<!-- <h2>Hello</h2>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+    in culpa qui officia deserunt mollit anim id est laborum.
+</p> -->
